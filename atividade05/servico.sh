cdb92d503f50c8d3312959e433de5bae6799d9e8
jamezaguiar <jamersonaguiar@alu.ufc.br>
2019-09-09 00:17:39 -0300

    servico.sh adjusted

--- atividade05/servico.sh
+++ atividade05/servico.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 COMANDO=$1
-if [ "$COMANDO" = "start" ]
-then
-	echo Iniciando Serviço
-elif [ "$COMANDO" = "stop" ]
-then
-	echo Parando Serviço
-elif [ "$COMANDO" = "restart" ]
-then
-	echo Reiniciando Serviço
-else
-	echo "Uso: ./servico.sh (start|stop|restart)"
-fi
+case $COMANDO in
+	"start") echo "Iniciando Serviço" ;;
+	"stop") echo "Parando Serviço" ;;
+	"restart") echo "Reiniciando Serviço" ;;
+	*) echo "Uso: ./servico.sh (start|stop|restart)" ;;
+esac
