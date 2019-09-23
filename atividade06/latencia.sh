5976b3b0b5e5ea8904969501831393cd719e8cff
jamezaguiar <jamersonaguiar@alu.ufc.br>
2019-09-23 10:10:10 -0300

    atividade06 completed

--- atividade06/latencia.sh
+++ atividade06/latencia.sh
@@ -2,10 +2,10 @@
 ARQUIVO=$1
 for ip in `cat $ARQUIVO`
 do
-	SOMA=`ping -c10 $ip | tail -n 14 | head -n 10 | cut -f7 -d' ' | cut -f2 -d'=' | tr '\n' '+' | sed 's/+$/\n/' | bc`
-	MEDIA=`echo "scale=2;$SOMA/10" | bc`
-	echo $ip >> resposta.txt
-	echo $MEDIA >> resposta.txt
+	MEDIA=`ping -c10 $ip | tail -n 1 | cut -f5 -d'/'`
+	echo $ip $MEDIA'ms'>> resposta.txt
 done
-cat resposta.txt
+cat resposta.txt | sort -k2 -n > respostaordenada.txt
+cat respostaordenada.txt
 rm resposta.txt
+rm respostaordenada.txt
