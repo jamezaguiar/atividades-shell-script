28dadaf36ec49bdf61e5d6260d931c559280f254
jamezaguiar <jamersonaguiar@alu.ufc.br>
2019-10-08 00:56:16 -0300

    testing atividade09

--- atividade09/hosts.sh
+++ atividade09/hosts.sh
@@ -1,66 +1,77 @@
 #!/bin/bash
 
-adicionar() {   
-   echo "$1:$2" >> hosts.db	
+adicionar() {
+   echo "$1:$2" >>hosts.db
 }
 
 remover() {
-   set -xv
-   sed -i '/^'$1'.*/d' hosts.db 	
-   set +xv
+   sed -i '/^'$1'.*/d' hosts.db
 }
 
 procurar() {
-   grep $1 hosts.db | cut -f2 -d':'	
+   grep $1 hosts.db | cut -f2 -d':'
 }
 
 reverso() {
-   grep $1 hosts.db | cut -f1 -d':'	
+   grep $1 hosts.db | cut -f1 -d':'
 }
 
 listar() {
-   cat hosts.db | tr ':' ' '	
+   cat hosts.db | tr ':' ' '
+}
+
+menu() {
+   printf "==================== MENU ====================\nAdicionar Host: ./hosts.sh -a <nome> -i <ip>\n\t\t./hosts.sh -i <ip> -a <nome>\nRemover Host:\t./hosts.sh -d <nome>\nBusca:\t\t./hosts.sh <nome>\nBusca Reversa:\t./hosts.sh -r <ip>\nListar Hosts:\t./hosts.sh -l\n"
+   read -p "Digite o comando desejado conforme instruções: " COMANDO
+   bash $COMANDO
 }
 
 getopts "a:i:r:d:l" OPTVAR
+
 case "$OPTVAR" in
-   "a") nome=$OPTARG
-	getopts "a:i:r:d:l" OPTVAR
-	if [ "$OPTVAR" == "i" ]
-        then
-           IP=$OPTARG
-        else    
-           echo "Está errado, babaca..."		
-	   exit 1
-        fi
-	adicionar $nome $IP
-	;;
-   "i") IP=$OPTARG
-	getopts "a:i:r:d:l" OPTVAR
-	if [ "$OPTVAR" == "a" ]
-        then
-           nome=$OPTARG
-        fi
-	adicionar $nome $IP
-	;;
-   "l") listar
-        exit 0     
-        ;;
-   "d") remover $OPTARG
-        ;;
-"r") reverso $OPTARG
-	exit 0
-	;;
-"") procurar $OPTARG
-	exit 0
-	;;
-*) echo Menu em construção!
+"a")
+   nome=$OPTARG
+   getopts "a:i:r:d:l" OPTVAR
+   if [ "$OPTVAR" == "i" ]; then
+      IP=$OPTARG
+   else
+      echo "USO CORRETO: -a <nome> -i <ip>"
+      exit 1
+   fi
+   adicionar $nome $IP
+   ;;
+"i")
+   IP=$OPTARG
+   getopts "a:i:r:d:l" OPTVAR
+   if [ "$OPTVAR" == "a" ]; then
+      nome=$OPTARG
+   else
+      "USO CORRETO: -i <ip> -a <nome>"
+      exit 1
+   fi
+   adicionar $nome $IP
+   ;;
+"l")
+   listar
+   exit 0
+   ;;
+"d")
+   remover $OPTARG
+   exit 0
+   ;;
+"?")
+   if [[ $1 =~ ^([A-Za-z0-9]+)$ ]]; then
+      procurar $1
+      exit 0
+   else
+      menu
+   fi
+   ;;
+"r")
+   reverso $OPTARG
+   exit 0
+   ;;
+*)
+   menu
+   ;;
 esac
-
-
-
-
-
-
-
-
